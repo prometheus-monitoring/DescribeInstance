f476538c63d5145657804383dc5b9950702a009a
lydat143 <lydat143@gmail.com>
2019-07-05 17:41:52 +0700

    Filter SE level 1

--- lib/vng_target.go
+++ lib/vng_target.go
@@ -7,21 +7,31 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+var (
+	SELV1 = [...]string{"phongnvd@vng.com.vn", "vihct@vng.com.vn", "truongln@vng.com.vn"}
+)
+
+type Data struct {
+	VMServerName string `json:"vm_server_name"`
+	NICS         []nic  `json:"nics"`
+	Owners       []se   `json:technical_owner`
+}
+
 type nic struct {
 	Card string    `json:"card"`
 	MAC  string    `json:"mac_address"`
 	Nets []network `json:"network"`
 }
 
-type Info struct {
-	VMServerName string `json:"VMServerName"`
-	NICS         []nic  `json:"NICS"`
-}
-
 type network struct {
 	VlanID   string `json:"vlan_id"`
 	VlanType string `json:"vlan_type"`
 	IP       string `json:"ip_address"`
+}
+
+type se struct {
+	Role  string   `json:"role"`
+	Users []string `json:"users"`
 }
 
 func connect() *sql.DB {
@@ -32,6 +42,15 @@ func connect() *sql.DB {
 	return db
 }
 
+func filter(name string) bool {
+	for _, owner := range SELV1 {
+		if name == owner {
+			return true
+		}
+	}
+	return false
+}
+
 func (ts Targets) GetTargetsVNG() []Target {
 	db := connect()
 	defer db.Close()
@@ -40,27 +59,39 @@ func (ts Targets) GetTargetsVNG() []Target {
 		panic(err.Error())
 	}
 
-	results, err := db.Query("SELECT VMServerName, NICS  FROM allserverinfo")
+	results, err := db.Query("SELECT Data FROM allserverinfo")
 	if err != nil {
 		panic(err.Error())
 	}
 
 	for results.Next() {
-		var info Info
-		var nics string
-		err = results.Scan(&info.VMServerName, &nics)
+		var data Data
+		var dataString string
+		err = results.Scan(&dataString)
 		if err != nil {
 			panic(err.Error())
 		}
-		err = json.Unmarshal([]byte(nics), &info.NICS)
+		err = json.Unmarshal([]byte(dataString), &data)
 		if err != nil {
 			panic(err.Error())
 		}
+
+		// Filter SE level 1
+		for _, owner := range data.Owners {
+			if owner.Role == "SE Level 1" {
+				for _, user := range owner.Users {
+					if filter(user) {
+						break
+					}
+				}
+			}
+		}
+
 		t := new(Target)
 		t.Labels = make(map[string]string)
 		// t.Labels["zone"] =
-		t.Labels["hostname"] = info.VMServerName
-		for _, nic := range info.NICS {
+		t.Labels["hostname"] = data.VMServerName
+		for _, nic := range data.NICS {
 			for _, net := range nic.Nets {
 				if net.VlanType == "public" && (t.Labels["ip"] == "" || t.Labels["ip_priv"] == "") {
 					t.Labels["ip"] = net.IP
