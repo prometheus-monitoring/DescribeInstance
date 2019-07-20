e99eb901a252b967569dbcf65b497f9ec6603a37
lydat143 <lydat143@gmail.com>
2019-07-20 17:50:19 +0700

    Change command to flag

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 	"strings"
 	"sync"
 
@@ -17,6 +16,9 @@ import (
 
 var (
 	locationsVN = [...]string{"HCM_QTSC_T1", "HCM_QTSC_T2", "Singapore"}
+	configPath  = kingpin.Flag("config.file", "DescribeInstance configuration file path.").Short(rune('c')).Default("config.yml").String()
+	manual      = kingpin.Flag("add.manual", "Add targets munual").Short(rune('m')).Default("false").Bool()
+	datacenter  = kingpin.Flag("datacenter", "Choose data center:\n\t all: Get all targets from the data center include aws, gcp, vng\n\t aws: Get all targets from the amazone web services\n\t gcp: Get all targets from the google cloud\n\t vng: Get all targets from the VN data center").Short(rune('d')).String()
 )
 
 func writeFile(content []byte, dir string) error {
@@ -36,19 +38,8 @@ func main() {
 	logLevel.Out = os.Stdout
 
 	//Parse flag
-	app := kingpin.New(filepath.Base(os.Args[0]), "Script get describe instance from cloud server")
-	app.HelpFlag.Short('h')
-	configPath := app.Flag("config.file", "DescribeInstance configuration file path.").Default("DecribeInstance.yml").String()
-	getFromAllCmd := app.Command("all", "Get describe instance from all aws, gcp, vng")
-	getFromAWSCmd := app.Command("aws", "Get describe instance from aws")
-	getFromGCPCmd := app.Command("gcp", "Get describe instance from gcp")
-	getFromVNGCmd := app.Command("vng", "Get describe instance from vng")
-	// configFiles := getFromGCPCmd.Arg(
-	// 	"config-files",
-	// 	"The config files to check.",
-	// ).Required().ExistingFiles()
-	parsedCmd := kingpin.MustParse(app.Parse(os.Args[1:]))
-	fmt.Println(*configPath)
+	kingpin.HelpFlag.Short(rune('h'))
+	kingpin.Parse()
 	// Read config
 	var conf config.Config
 	conf.NewConfig(logLevel, *configPath)
@@ -59,86 +50,24 @@ func main() {
 
 	desDir := "/etc/prometheus/targets/"
 	ensureDir(desDir)
-
-	switch parsedCmd {
-	case getFromAllCmd.FullCommand():
-		wg.Add(3)
-		fallthrough
-	case getFromAWSCmd.FullCommand():
-		if !strings.Contains(parsedCmd, "all") {
-			wg.Add(1)
-		}
-		go func() {
-			defer wg.Done()
-			targets, err := ts.GetTargetsAWS(logLevel)
-			if err == nil {
-				content, _ := json.MarshalIndent(targets, "", "\t")
-				fileDir := desDir + "targets_aws.json"
-				logLevel.Info("Write all targets on aws to json file")
-				err = writeFile(content, fileDir)
-				if err != nil {
-					logLevel.Error(err)
-				} else {
-					logLevel.Info("Write targets on datacenter vng completed")
-				}
-			} else {
-				logLevel.Error(err)
+	if *manual {
+		ts.AddManual()
+	} else {
+		switch *datacenter {
+		case "all":
+			wg.Add(3)
+			fallthrough
+		case "aws":
+			if *datacenter != "all" {
+				wg.Add(1)
 			}
-		}()
-		if !strings.Contains(parsedCmd, "all") {
-			break
-		}
-		fallthrough
-	case getFromGCPCmd.FullCommand():
-		if !strings.Contains(parsedCmd, "all") {
-			wg.Add(1)
-		}
-		go func() {
-			defer wg.Done()
-			targets, err := ts.GetTargetsGCP(logLevel)
-			if err == nil {
-				content, _ := json.MarshalIndent(targets, "", "\t")
-				fileDir := desDir + "targets_gcp.json"
-				logLevel.Info("Write all targets on gcp to json file")
-				err = writeFile(content, fileDir)
-				if err != nil {
-					logLevel.Error(err)
-				} else {
-					logLevel.Info("Write targets on datacenter vng completed")
-				}
-			} else {
-				logLevel.Error(err)
-			}
-		}()
-		if !strings.Contains(parsedCmd, "all") {
-			break
-		}
-		fallthrough
-	case getFromVNGCmd.FullCommand():
-		if !strings.Contains(parsedCmd, "all") {
-			wg.Add(1)
-		}
-		go func() {
-			defer wg.Done()
-			logLevel.Info("[vng] Establishing connection to database")
-			db, err := ts.Connect(conf.Creds.MySQL)
-			defer db.Close()
-			if err != nil {
-				logLevel.Panic(err)
-			}
-			for _, location := range locationsVN {
-				targets, err := ts.GetTargetsVNG(logLevel, db, location, conf.Filter)
+			go func() {
+				defer wg.Done()
+				targets, err := ts.GetTargetsAWS(logLevel)
 				if err == nil {
 					content, _ := json.MarshalIndent(targets, "", "\t")
-					var fileDir string
-					if strings.Contains(location, "T1") {
-						fileDir = fmt.Sprintf("%stargets_vng_%s.json", desDir, "oldfarm")
-					} else if strings.Contains(location, "T2") {
-						fileDir = fmt.Sprintf("%stargets_vng_%s.json", desDir, "newfarm")
-					} else {
-						fileDir = fmt.Sprintf("%stargets_vng_%s.json", desDir, "singapore")
-					}
-					logLevel.Info("Write all targets on datacenter vng to json file")
+					fileDir := desDir + "targets_aws.json"
+					logLevel.Info("Write all targets on aws to json file")
 					err = writeFile(content, fileDir)
 					if err != nil {
 						logLevel.Error(err)
@@ -148,8 +77,73 @@ func main() {
 				} else {
 					logLevel.Error(err)
 				}
+			}()
+			if *datacenter == "all" {
+				break
 			}
-		}()
+			fallthrough
+		case "gcp":
+			if *datacenter != "all" {
+				wg.Add(1)
+			}
+			go func() {
+				defer wg.Done()
+				targets, err := ts.GetTargetsGCP(logLevel)
+				if err == nil {
+					content, _ := json.MarshalIndent(targets, "", "\t")
+					fileDir := desDir + "targets_gcp.json"
+					logLevel.Info("Write all targets on gcp to json file")
+					err = writeFile(content, fileDir)
+					if err != nil {
+						logLevel.Error(err)
+					} else {
+						logLevel.Info("Write targets on datacenter vng completed")
+					}
+				} else {
+					logLevel.Error(err)
+				}
+			}()
+			if *datacenter == "all" {
+				break
+			}
+			fallthrough
+		case "vng":
+			if *datacenter != "all" {
+				wg.Add(1)
+			}
+			go func() {
+				defer wg.Done()
+				logLevel.Info("[vng] Establishing connection to database")
+				db, err := ts.Connect(conf.Creds.MySQL)
+				defer db.Close()
+				if err != nil {
+					logLevel.Panic(err)
+				}
+				for _, location := range locationsVN {
+					targets, err := ts.GetTargetsVNG(logLevel, db, location, conf.Filter)
+					if err == nil {
+						content, _ := json.MarshalIndent(targets, "", "\t")
+						var fileDir string
+						if strings.Contains(location, "T1") {
+							fileDir = fmt.Sprintf("%stargets_vng_%s.json", desDir, "oldfarm")
+						} else if strings.Contains(location, "T2") {
+							fileDir = fmt.Sprintf("%stargets_vng_%s.json", desDir, "newfarm")
+						} else {
+							fileDir = fmt.Sprintf("%stargets_vng_%s.json", desDir, "singapore")
+						}
+						logLevel.Info("Write all targets on datacenter vng to json file")
+						err = writeFile(content, fileDir)
+						if err != nil {
+							logLevel.Error(err)
+						} else {
+							logLevel.Info("Write targets on datacenter vng completed")
+						}
+					} else {
+						logLevel.Error(err)
+					}
+				}
+			}()
+		}
 	}
 	wg.Wait()
 }
